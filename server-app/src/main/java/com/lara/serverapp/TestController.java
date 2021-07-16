88f13943308db9f1d596e2a304d61c6d2d034d73
vijaytripathi553 <vijaytripathi553@gmail.com>
2021-07-16 08:36:45 +0530

    server-app

---
+++ server-app/src/main/java/com/lara/serverapp/TestController.java
@@ -0,0 +1,15 @@
+package com.lara.serverapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@GetMapping("/test1")
+	public String test1()
+	{
+		System.out.println("from server-app -test1() service");
+		return "from server-app -test1() service";
+	}
+
+}
